131ef3e518ce76cf797ad4ce050f099bc2bd492d
NicolasZysman <nzysman@gmail.com>
2022-10-12 20:56:30 -0300

    Crea pagina Detail

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const GET_SEARCH = "GET_SEARCH";
 export const SEARCH_COUNTRY = "SEARCH_COUNTRY";
+export const GET_DETAIL = "GET_DETAIL";
+export const CLEAR_DETAIL = "CLEAR_DETAIL";
 
 export const searchCountry = (payload) => {
   return async (dispatch) => {
@@ -14,3 +16,23 @@ export const searchCountry = (payload) => {
       );
   };
 };
+
+export function getDetail(id){
+  return async function (dispatch) {
+      try {
+          var json = await axios.get(`http://localhost:3001/acavaunaruta/${id}`)
+          return dispatch({
+              type: 'GET_DETAIL',
+              payload: json.data
+          })
+      } catch (err) {
+          console.log(err)
+      }
+  }
+}
+
+export function clearDetail(){
+  return {
+      type: 'CLEAR_DETAIL',
+  }
+}
