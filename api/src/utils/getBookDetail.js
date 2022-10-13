61ae17a7529e1365e3a6d17738093c9e76a11f5d
ramirogz1 <ramirogz101@gmail.com>
2022-10-12 21:18:09 -0300

    feat0005-Prueba-getBookDetail

--- api/src/utils/getBookDetail.js
+++ api/src/utils/getBookDetail.js
@@ -1,17 +1,25 @@
-require('dotenv').config();
-const axios = require('axios');
-const { } = require('../db');
-const { API_KEY } = process.env;
+require("dotenv").config();
+const axios = require("axios");
+const { Books, Genres } = require("../db");
 
 async function getBookDetail(idBook) {
-
+  const idDb = await Books.findByPk(id, {
+    include: Genres,
+  });
+  const ApiUrl = await axios("https://");
+  const detailBook = await ApiUrl.data.map((e) => {
+    return {
+      name: e.name,
+      author: e.author,
+      image: e.image,
+      description: e.description,
+      price: e.price,
+      editorial: e.editorial,
+    };
+  });
+  return detailBook;
 }
 
 module.exports = {
-  getBookDetail
-}
-
-
-
-
-
+  getBookDetail,
+};
