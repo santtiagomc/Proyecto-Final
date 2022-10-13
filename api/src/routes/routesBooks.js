30933c7ea62141d3b06324e71075d34cb8cd39e3
Eli-zabethh00 <95299888+Eli-zabethh00@users.noreply.github.com>
2022-10-13 10:22:13 -0300

    GetAllBooks, routes y utils

--- api/src/routes/routesBooks.js
+++ api/src/routes/routesBooks.js
@@ -7,8 +7,14 @@ const { createBook } = require('../utils/createBook')
 const router = Router();
 
 router.get('/', async (req, res) => {
+    const {name} = req.query;
+    const books = await getBooks(name);
 
-})
+    books.messageError? 
+    res.status(404).json(books):
+    res.status(201).json(books);
+
+});
 
 router.post("/", async (req, res) => {
 
--- api/src/utils/getBooks.js
+++ api/src/utils/getBooks.js
@@ -1,12 +1,33 @@
-require('dotenv').config();
 const axios = require('axios');
-const { } = require('../db');
-const { API_KEY } = process.env;
+const { Books, Genres } = require('../db');
 
 async function getBooks(name) {
+  try{
+    const allBooks = await Books.findAll({
+      include: [{
+        model: Genres,
+        attributes: ['name'],
+        through: { attributes: [] }
+      }]
+    });
 
-}
+    if(name){
+      const books = allBooks.filter(b => b.name.toLowerCase().includes(name.toLowerCase()));
+      //{
+      // if(!b.eliminated){
+        //     return b.name.toLowerCase().includes(name.toLowerCase())
+        // }
+        //});
+      if(!books.length) return {messageError: 'No se encontraron coincidencias.'}
+      return books;
+    }else{
+      return allBooks;
+    }
+  }catch(error){
+    return {messageError: 'Se ha producido un error.'}
+  };
+};
 
 module.exports = {
   getBooks
-}
+};
