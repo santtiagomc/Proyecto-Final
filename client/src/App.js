7e7d9a9df5302aaee3b16274dfcc0022135d0dc4
Santiago Moreno <santtiagomc@gmail.com>
2022-10-14 13:52:15 -0500

    form creacion books funcionando

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import AddBooks from "./pages/AddBooks"
+import AddBooks from "./pages/CreateBook/AddBooks"
 
 import Home from "./pages/Home/Home.jsx";
 import Detail from "./pages/Detail/Detail.jsx";
