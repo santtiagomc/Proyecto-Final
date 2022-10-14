c38e3b1411e32e70975d2d77b16db0737ecb0f52
German1138 <germancourvoisier1138@gmail.com>
2022-10-13 22:16:45 -0300

    feat(/home):Estructurados

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,29 +1,40 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import SearchBar from "../../components/SearchBar/SearchBar.jsx";
-import Cards from "../../components/Cards/Cards.jsx";
+import Card from "../../components/Card/Card.jsx";
 
 import style from "./Home.module.css";
-import Card from "../../components/Card/Card.jsx";
 import api from "../../api.js";
 
 export default function Home() {
   //const allBooks = useSelector((state) => state.books);
+  const dispatch = useDispatch();
   const allBooks = api.books;
+
+  useEffect(() => {
+    /* if(!allBooks.length) dispatch(getBooks()) */
+  }, []);
 
   return (
     <>
-      <SearchBar />
-      {/* //navbar */}
+      <header>
+        <SearchBar />
+      </header>
+      {/* //aca el navbar */}
 
-      <div className={style.container}>
-        <Cards />
-      </div>
-      <div className={style.container}>
+      <div className={style.grid}>
         {allBooks.length ? (
           allBooks.map((el, index) => {
-            return <Card key={index} image={el.image} price={el.price} />;
+            return (
+              <Card
+                key={index}
+                image={el.image}
+                price={el.price}
+                name={el.name}
+                author={el.author}
+              />
+            );
           })
         ) : (
           <span className={style.span}>404 not found</span>
