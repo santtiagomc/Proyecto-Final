8d4a6d29daff67c8a84e9df4c558e76397e35c7a
Juliana <julianagon73@gmail.com>
2022-10-16 20:41:46 -0300

    Estilos de NavBar y Filtros terminados

--- client/src/components/NavBar/FiltersNav.jsx
+++ client/src/components/NavBar/FiltersNav.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeFilter, changePage, changeSearch } from "../../redux/actions";
-
-import style from "../../styles/FiltersNav.module.css";
+import style from "./FiltersNav.module.css";
 
 export default function FiltersNav({ editorials }) {
 
@@ -27,48 +26,101 @@ export default function FiltersNav({ editorials }) {
   };
 
   return (
-    <>
-      <nav>
-        <h2>Filtros</h2>
-        <label>Ordenar:</label>
-        <select
-          name="sort"
-          onChange={handleFilterSort}
-        >
-          <option disabled>-- Nombre del libro --</option>
-          <option value="A-Z" selected={filtersApplied.sort === "A-Z" ? true : false}>A - Z</option>
-          <option value="Z-A" selected={filtersApplied.sort === "Z-A" ? true : false}>Z - A</option>
-          <option disabled>-- Precio --</option>
-          <option value="price-min-max" selected={filtersApplied.sort === "price-min-max" ? true : false}>Menor a mayor</option>
-          <option value="price-max-min" selected={filtersApplied.sort === "price-max-min" ? true : false}>Mayor a menor</option>
-          <option disabled>-- Año de edición --</option>
-          <option value="edition-min-max" selected={filtersApplied.sort === "edition-min-max" ? true : false}>Menor a mayor</option>
-          <option value="edition-max-min" selected={filtersApplied.sort === "edition-max-min" ? true : false}>Mayor a menor</option>
-        </select>
-
-        <label>Filtrar por editoriales:</label>
-        <select
-          name="editorial"
-          onChange={handleFilterEditorial}>
-          <option disabled > -- Filtrar por editorial --</option>
-          <option value="none" selected={filtersApplied.editorial === "none" ? true : false}> Todas las editoriales</option>
-          {editorials && editorials.map((el) => <option value={el} selected={filtersApplied.editorial === el ? true : false}>{el}</option>)}
-        </select>
-
-        <label>Filtrar por género:</label>
-        <select
-          name="genres"
-          onChange={handleFilterGenres}
-        >
-          <option disabled>-- Filtrar por género --</option>
-          <option value="none" selected={filtersApplied.genres === "none" ? true : false}> Todos los géneros</option>
-          {genres &&
-            genres.sort().map((el) => <option value={el} selected={filtersApplied.genres === el ? true : false}>{el}</option>)}
-        </select>
-
-        <label>Filtrar por género:</label>
-        <button onClick={reset}>Resetear todo</button>
-      </nav>
-    </>
-  );
+			<>
+				<nav className={style.container}>
+					<h2 className={style.filters}>Filtros</h2>
+					<label className={style.label}>Ordenar:</label>
+					<select name="sort" onChange={handleFilterSort} className={style.select}>
+						<option disabled>-- Nombre del libro --</option>
+						<option
+							value="A-Z"
+							selected={filtersApplied.sort === "A-Z" ? true : false}
+						>
+							A - Z
+						</option>
+						<option
+							value="Z-A"
+							selected={filtersApplied.sort === "Z-A" ? true : false}
+						>
+							Z - A
+						</option>
+						<option disabled>-- Precio --</option>
+						<option
+							value="price-min-max"
+							selected={filtersApplied.sort === "price-min-max" ? true : false}
+						>
+							Menor a mayor
+						</option>
+						<option
+							value="price-max-min"
+							selected={filtersApplied.sort === "price-max-min" ? true : false}
+						>
+							Mayor a menor
+						</option>
+						<option disabled>-- Año de edición --</option>
+						<option
+							value="edition-min-max"
+							selected={filtersApplied.sort === "edition-min-max" ? true : false}
+						>
+							Menor a mayor
+						</option>
+						<option
+							value="edition-max-min"
+							selected={filtersApplied.sort === "edition-max-min" ? true : false}
+						>
+							Mayor a menor
+						</option>
+					</select>
+					<label className={style.label}>Filtrar por editoriales:</label>
+					<select
+						name="editorial"
+						onChange={handleFilterEditorial}
+						className={style.select}
+					>
+						<option disabled> -- Filtrar por editorial --</option>
+						<option
+							value="none"
+							selected={filtersApplied.editorial === "none" ? true : false}
+						>
+							{" "}
+							Todas las editoriales
+						</option>
+						{editorials &&
+							editorials.map((el) => (
+								<option
+									value={el}
+									selected={filtersApplied.editorial === el ? true : false}
+								>
+									{el}
+								</option>
+							))}
+					</select>
+					<label className={style.label}>Filtrar por género:</label>
+					<select
+						name="genres"
+						onChange={handleFilterGenres}
+						className={style.select}
+					>
+						<option disabled>-- Filtrar por género --</option>
+						<option
+							value="none"
+							selected={filtersApplied.genres === "none" ? true : false}
+						>
+							{" "}
+							Todos los géneros
+						</option>
+						{genres &&
+							genres.sort().map((el) => (
+								<option
+									value={el}
+									selected={filtersApplied.genres === el ? true : false}
+								>
+									{el}
+								</option>
+							))}
+					</select>
+					<button onClick={reset} className={style.button}>Resetear todo</button>
+				</nav>
+			</>
+		);
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,74 +4,89 @@ import { Link } from "react-router-dom";
 import SearchBar from "../../components/SearchBar/SearchBar.jsx";
 import Card from "../../components/Card/Card.jsx";
 import FiltersNav from "../../components/NavBar/FiltersNav.jsx";
-import { getGenres, searchBook, changePage, changeFilter, changeSearch, getEditorials } from "../../redux/actions";
-
+import {
+	getGenres,
+	searchBook,
+	changePage,
+	changeFilter,
+	changeSearch,
+	getEditorials,
+} from "../../redux/actions";
+import NavBar from "../../components/NavBar/NavBar.jsx";
 import style from "./HomePrueba.module.css";
 
 export default function Home() {
-  const { filtersApplied, searchApplied, genres, page, total, editorials, books } =
-    useSelector((state) => state);
-  const dispatch = useDispatch();
-  const pages = [];
+	const {
+		filtersApplied,
+		searchApplied,
+		genres,
+		page,
+		total,
+		editorials,
+		books,
+	} = useSelector((state) => state);
+	const dispatch = useDispatch();
+	const pages = [];
 
-  useEffect(() => {
-    if (!genres.length) dispatch(getGenres());
-    if (!editorials.length) dispatch(getEditorials());
-    dispatch(searchBook(filtersApplied, searchApplied, page));
-  }, [filtersApplied, page, searchApplied]);
+	useEffect(() => {
+		if (!genres.length) dispatch(getGenres());
+		if (!editorials.length) dispatch(getEditorials());
+		dispatch(searchBook(filtersApplied, searchApplied, page));
+	}, [filtersApplied, page, searchApplied]);
 
-  const nextPage = () => {
-    if (page + 10 < total) {
-      dispatch(changePage(page + 10));
-    }
-  };
+	const nextPage = () => {
+		if (page + 10 < total) {
+			dispatch(changePage(page + 10));
+		}
+	};
 
-  const prevPage = () => {
-    if (page > 0) {
-      dispatch(changePage(page - 10));
-    }
-  };
+	const prevPage = () => {
+		if (page > 0) {
+			dispatch(changePage(page - 10));
+		}
+	};
 
-  const handlePage = (page) => {
-    dispatch(changePage(page * 10 - 10));
-  };
+	const handlePage = (page) => {
+		dispatch(changePage(page * 10 - 10));
+	};
 
-  for (let i = 1; i <= Math.ceil(total / 10); i++) {
-    pages.push(i);
-  }
+	for (let i = 1; i <= Math.ceil(total / 10); i++) {
+		pages.push(i);
+	}
 
-  return (
-    <>
-      <header>
-        <SearchBar />
-      </header>
-      {/* //aca el navbar */}
-      <FiltersNav editorials={editorials} />
-      <div>
-        <button onClick={prevPage}>Anterior</button>
-        {pages.map((page) => (
-          <button onClick={() => handlePage(page)}>{page}</button>
-        ))}
-        <button onClick={nextPage}>Siguiente</button>
-      </div>
-      <div className={style.grid}>
-        {!books.messageError ? (
-          books.map((el, index) => {
-            return (
-              <Card
-                key={index}
-                id={el.id}
-                image={el.image}
-                price={el.price}
-                name={el.name}
-                author={el.author}
-              />
-            );
-          })
-        ) : (
-          <span className={style.span}>{books.messageError}</span>
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<header>
+				<NavBar />
+				{/* <SearchBar /> */}
+			</header>
+
+			<FiltersNav editorials={editorials} />
+			<div>
+				<button onClick={prevPage}>Anterior</button>
+				{pages.map((page) => (
+					<button onClick={() => handlePage(page)}>{page}</button>
+				))}
+				<button onClick={nextPage}>Siguiente</button>
+			</div>
+			<div className={style.grid}>
+				{!books.messageError ? (
+					books.map((el, index) => {
+						return (
+							<Card
+								key={index}
+								id={el.id}
+								image={el.image}
+								price={el.price}
+								name={el.name}
+								author={el.author}
+							/>
+						);
+					})
+				) : (
+					<span className={style.span}>{books.messageError}</span>
+				)}
+			</div>
+		</>
+	);
 }
