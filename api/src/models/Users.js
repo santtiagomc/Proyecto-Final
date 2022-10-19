8406cdde3aa5e0668664259942023a6209dc4323
Facug03 <facundogonzales1550@gmail.com>
2022-10-19 15:59:25 -0300

    usuario baneado y role

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -2,37 +2,49 @@ const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define("Users", {
-    id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-    },
-    fullName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    province: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    city: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    zipCode: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-  }, {
-    timestamps: false,
-  });
+	// defino el modelo
+	sequelize.define(
+		"Users",
+		{
+			id: {
+				type: DataTypes.STRING,
+				primaryKey: true,
+			},
+			fullName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			province: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			city: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			address: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			zipCode: {
+				type: DataTypes.INTEGER,
+				allowNull: true,
+			},
+			role: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			status: {
+				type: DataTypes.BOOLEAN,
+				allowNull: true,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	);
 };
