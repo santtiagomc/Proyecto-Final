f41d74a0a8af9a09f547f01aa76ee66d42f4cb96
Facug03 <facundogonzales1550@gmail.com>
2022-10-21 10:18:14 -0300

    manejo de algunos errorores

--- client/src/firebase/auth.js
+++ client/src/firebase/auth.js
@@ -1,85 +1,59 @@
 import {
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	signOut,
-	GoogleAuthProvider,
-	signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { auth } from "./firebase";
 import axios from "axios";
 
-export const singUp = async ({
-	email,
-	password,
-	fullName,
-	province,
-	city,
-	address,
-	zipCode,
-}) => {
-	try {
-		const userAuth = await createUserWithEmailAndPassword(
-			auth,
-			email,
-			password
-		);
-		console.log(userAuth);
-		await axios.post("http://localhost:3001/user", {
-			id: userAuth.user.uid,
-			email,
-			fullName,
-			province,
-			city,
-			address,
-			zipCode: Number(zipCode),
-		});
-	} catch (error) {
-		console.log(error.message);
-	}
+export const singUp = async ({ email, password, fullName }) => {
+  try {
+    const userAuth = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await axios.post("http://localhost:3001/user", {
+      id: userAuth.user.uid,
+      email,
+      fullName,
+    });
+  } catch (error) {
+    console.log(error);
+    return "Un error ha ocurrido";
+  }
 };
 
 export const singIn = (email, password) =>
-	signInWithEmailAndPassword(auth, email, password);
+  signInWithEmailAndPassword(auth, email, password);
 
 export const sessionGoogle = async () => {
-	const googleProvider = new GoogleAuthProvider();
-	await singInGoogle(googleProvider);
+  const googleProvider = new GoogleAuthProvider();
+  await singInGoogle(googleProvider);
 };
 
 const singInGoogle = async (googleProvider) => {
-	try {
-		const res = await signInWithPopup(auth, googleProvider);
-		const userExists = await axios.get(
-			`http://localhost:3001/user/${res.user.uid}`
-		);
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const userExists = await axios.get(
+      `http://localhost:3001/user/${res.user.uid}`
+    );
 
-		if (userExists.data.message) {
-			await axios.post("http://localhost:3001/user", {
-				id: res.user.uid,
-				fullName: res.user.displayName,
-				email: res.user.email,
-			});
-		}
-	} catch (error) {
-		console.error(error);
-	}
+    if (userExists.data.message) {
+      await axios.post("http://localhost:3001/user", {
+        id: res.user.uid,
+        fullName: res.user.displayName,
+        email: res.user.email,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return "Un error ha ocurrido";
+  }
 };
 
 export const logOut = async () => {
-	await signOut(auth);
+  await signOut(auth);
 };
-
-// export const userExist = async (user) => {
-// 	try {
-// 		const res = await axios.get(`http://localhost:3001/user/${user.uid}`);
-// 		console.log(res.data);
-// 		if (!res.messageError) {
-// 			dispatch(userExist(user));
-// 		} else {
-// 			dispatch(userExist(user));
-// 		}
-// 	} catch (error) {
-// 		console.log(error);
-// 		return error;
-// 	}
-// };
