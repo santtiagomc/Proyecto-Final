35a436ad0feee087615c0b58913b4ebc3fda77aa
ramirogz1 <ramirogz101@gmail.com>
2022-10-22 01:41:46 -0300

    pruebaDeploy

---
+++ api/config.js
@@ -0,0 +1,15 @@
+const PORT = process.env.PORT  
+const DB_USER = process.env.DB_USER  
+const DB_PASSWORD = process.env.DB_PASSWORD  
+const DB_HOST = process.env.DB_HOST  
+const DB_PORT = process.env.DB_PORT  
+const DB_NAME = process.env.DB_NAME  
+
+module.exports = {
+  PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME,
+  DB_PORT 
+}
--- api/src/db.js
+++ api/src/db.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgresql://postgres:qkU2qe0YbFrpMwkUrv3l@containers-us-west-62.railway.app:6032/railway`,
+  `postgresql://${process.env.PGUSER }:${ process.env.PGPASSWORD }@${ process.env.PGHOST }:${ process.env.PGPORT }/${ process.env.PGDATABASE }`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
