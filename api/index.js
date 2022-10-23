287215f51404a2bc9919847a71bf3ccbf083214c
Santiago Moreno <santtiagomc@gmail.com>
2022-10-23 12:44:46 -0500

    cambios para deploy 2

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
+const { conn, sequelize } = require("./src/db.js");
 const { PORT } = require("./config.js")
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
-  });
-});
+// conn.sync({ force: false }).then(() => {
+//   server.listen(PORT, () => {
+//     console.log("%s listening at 3001"); // eslint-disable-line no-console
+//   });
+// });
+sequelize.sync().then(() => {
+  server.listen(PORT)
+})
