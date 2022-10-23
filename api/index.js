2bff0a3fb2a32c60e21818286b48827212b7ac00
Santiago Moreno <santtiagomc@gmail.com>
2022-10-23 12:42:51 -0500

    cambios para deploy

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const { PORT } = require("./config.js")
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(6980, () => {
+  server.listen(PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
