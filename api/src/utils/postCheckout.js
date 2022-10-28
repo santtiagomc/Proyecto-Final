06791dc11da58b7ef6dd12d561856f52de968f08
Facug03 <facundogonzales1550@gmail.com>
2022-10-28 17:11:42 -0300

    identado

--- api/src/utils/postCheckout.js
+++ api/src/utils/postCheckout.js
@@ -11,6 +11,7 @@ async function postCheckout({ cart, stripeId }) {
     const total = Math.round(
       cart.reduce((acc, act) => acc + Number(act.price), 0)
     );
+
     const payment = await stripe.paymentIntents.create({
       amount: total * 100,
       currency: "USD",
@@ -20,16 +21,13 @@ async function postCheckout({ cart, stripeId }) {
 
     cartBuy.status = "Cerrado";
     await cartBuy.save();
+
     cart.map(async (cart) => {
       const findBook = await Books.findByPk(cart.id);
       findBook.stock = findBook.stock - cart.quantity;
       await findBook.save();
-      // const findBook = cart.find((book) => book.id === books.dataValues.id);
-      // console.log(findBook.quantity);
-      // console.log(books.dataValues.stock);
-      // books.dataValues.stock = books.dataValues.stock - findBook.quantity;
-      // await books.save();
     });
+
     return { message: "Pago realizado correctamente" };
   } catch (error) {
     console.log(error);
