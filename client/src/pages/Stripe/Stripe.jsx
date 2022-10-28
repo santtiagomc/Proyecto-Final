816daa62b31eeffb4bd942e8aa8f86f43a6612e5
NicolasZysman <nzysman@gmail.com>
2022-10-28 15:05:37 -0300

    .

--- client/src/pages/Stripe/Stripe.jsx
+++ client/src/pages/Stripe/Stripe.jsx
@@ -1,40 +1,47 @@
 import React from "react";
-import { loadStripe } from "@stripe/stripe-js"
-import { Elements, CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
- 
-import "./Stripe.module.css";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
-const stripePromsie = loadStripe("pk_test_51LwtGzGm2004ZMTNJJTuwMAa47xnU7d3mDoI861T6xSLy0Y2eFsvDzDK5k3RgHGk8WOW711HbHNy6GvCDog76CgJ00GhqRZpD8")
+import style from "./Stripe.module.css";
+
+const stripePromsie = loadStripe(
+  "pk_test_51LwtGzGm2004ZMTNJJTuwMAa47xnU7d3mDoI861T6xSLy0Y2eFsvDzDK5k3RgHGk8WOW711HbHNy6GvCDog76CgJ00GhqRZpD8"
+);
 
 export default function Stripe() {
-
-    const CheckoutForm = () => {
-
-        const stripe = useStripe();
-        const elements = useElements();
-        return <form className="form-group"
-        onSubmit={handleSubmit}>
-            <CardElement className="from-control"/>
-            <button>
-                Comprar
-            </button>
-        </form>
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log("HOla")
-
-    //    Esto de abajo para el back, hay que hacer la action
-        const { error, paymentMethod } = await Stripe.createPaymentMethod({
-            type: "card",
-            card: Elements.getElement(CardElement) 
-        })
-    }
-
-    return (
-            <Elements stripe={stripePromsie} >
-                <CheckoutForm />
-            </Elements>
-    )
+  return (
+    <div className={style.container}>
+      <Elements stripe={stripePromsie}>
+        <CheckoutForm />
+      </Elements>
+    </div>
+  );
 }
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //    Esto de abajo para el back, hay que hacer la action
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+
+    // if (!error)
+    console.log(error, paymentMethod);
+  };
+  return (
+    <form className={style.stripeControl} onSubmit={handleSubmit}>
+      <CardElement />
+      <button>Comprar</button>
+    </form>
+  );
+};
