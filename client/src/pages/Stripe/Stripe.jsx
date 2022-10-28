54a2a2d8f68b548afcccc7e2b331db2ac41e432e
Facug03 <facundogonzales1550@gmail.com>
2022-10-28 17:04:55 -0300

    Pasarela de pago

--- client/src/pages/Stripe/Stripe.jsx
+++ client/src/pages/Stripe/Stripe.jsx
@@ -6,6 +6,8 @@ import {
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 import style from "./Stripe.module.css";
 
@@ -14,29 +16,49 @@ const stripePromsie = loadStripe(
 );
 
 export default function Stripe() {
+  const cart = useSelector((state) => state.cart);
+
+  console.log(cart);
   return (
     <div className={style.container}>
+      {/* <div>
+        {cart.length &&
+          Math.round(cart.reduce((acc, act) => acc + Number(act.price), 0))}
+      </div> */}
       <Elements stripe={stripePromsie}>
-        <CheckoutForm />
+        <CheckoutForm cart={cart} />
       </Elements>
     </div>
   );
 }
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ cart }) => {
   const stripe = useStripe();
   const elements = useElements();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     //    Esto de abajo para el back, hay que hacer la action
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
-    });
+    try {
+      const { error, paymentMethod } = await stripe.createPaymentMethod({
+        type: "card",
+        card: elements.getElement(CardElement),
+      });
 
-    // if (!error)
-    console.log(error, paymentMethod);
+      if (!error) {
+        const { id } = paymentMethod;
+        console.log(id);
+        console.log(paymentMethod);
+        const res = await axios.post("http://localhost:3001/checkout", {
+          stripeId: id,
+          cart: cart,
+        });
+        console.log(res);
+      }
+    } catch (error) {
+      console.log(error);
+      console.log(error.response?.data.messageError);
+    }
   };
   return (
     <form className={style.stripeControl} onSubmit={handleSubmit}>
