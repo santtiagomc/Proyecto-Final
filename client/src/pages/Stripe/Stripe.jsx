8c57a9f2f0ca24c8a4f08eb1d79ecb3fc307eff3
Facug03 <facundogonzales1550@gmail.com>
2022-10-28 21:47:40 -0300

    cambios

--- client/src/pages/Stripe/Stripe.jsx
+++ client/src/pages/Stripe/Stripe.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   Elements,
@@ -7,7 +7,9 @@ import {
   useElements,
 } from "@stripe/react-stripe-js";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 import style from "./Stripe.module.css";
 
@@ -35,6 +37,9 @@ export default function Stripe() {
 const CheckoutForm = ({ cart }) => {
   const stripe = useStripe();
   const elements = useElements();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -44,7 +49,7 @@ const CheckoutForm = ({ cart }) => {
         type: "card",
         card: elements.getElement(CardElement),
       });
-
+      setLoading(true);
       if (!error) {
         const { id } = paymentMethod;
         console.log(id);
@@ -53,17 +58,48 @@ const CheckoutForm = ({ cart }) => {
           stripeId: id,
           cart: cart,
         });
+        // history.push("/profile");
         console.log(res);
+        Swal.fire({
+          title: "Producto comprado correctamente",
+          width: 650,
+          icon: "success",
+          iconColor: "#355070",
+          background: "#19191a",
+          color: "#e1e1e1",
+          confirmButtonColor: "#355070",
+          confirmButtonText: `<a href="http://localhost:3000/profile">Aceptar</a>`,
+        });
       }
     } catch (error) {
       console.log(error);
-      console.log(error.response?.data.messageError);
+      if (error.response) {
+        if (
+          error.response.data.messageError ===
+          "Your card was declined. Your request was in test mode, but used a non test (live) card. For a list of valid test cards, visit: https://stripe.com/docs/testing."
+        )
+          setError("Esta tarjeta fue declinada");
+      }
+      if (error.response.data.messageError === "Your card was declined.")
+        setError("Tu tarjeta fue rechazada");
+
+      // setError(error.response.data.messageError);
     }
+    setLoading(false);
   };
   return (
     <form className={style.stripeControl} onSubmit={handleSubmit}>
-      <CardElement />
-      <button>Comprar</button>
+      <CardElement className={style.pay} />
+      {error && <p className={style.err}>{error}</p>}
+      <button>
+        {loading ? (
+          <div className={style.loaderContainer}>
+            <span className={style.loader}></span>
+          </div>
+        ) : (
+          "Comprar"
+        )}
+      </button>
     </form>
   );
 };
